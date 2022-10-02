85b34c82f31d1ec3fd55c5e1ac06a68883c1d055
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2022-10-02 12:22:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', (req, res) => {
+  return res.json({ 
+    ipaddress: req.ip, 
+    language: req.headers["accept-language"], 
+    software: req.headers["user-agent"] 
+  });
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
